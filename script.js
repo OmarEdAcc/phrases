384bac1ee7cb638e4e78b87e18e1f2cf1422f5d4
OmarEdAcc <160067097+OmarEdAcc@users.noreply.github.com>
2024-03-20 13:32:55 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+document.getElementById("disguise").addEventListener("click", function(){
+    alert("disguise")
+})
+
+document.getElementById("dozen").addEventListener("click", function(){
+    alert("dozen")
+})
+
+document.getElementById("never").addEventListener("click", function(){
+    alert("never")
+})
+
+document.getElementById("gain").addEventListener("click", function(){
+    alert("gain")
+})
+
+document.getElementById("by").addEventListener("click", function(){
+    alert("by")
+})
